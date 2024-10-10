c22cbced531e1ae53a8ac2ed7994c5f3c0ffeef9
bbukowsk <99203364+bbukowsk@users.noreply.github.com>
2024-10-10 12:26:11 -0400

    Update QHO_AIMD_entropy.py
    v1.0 notes:
     - removed DOFs as an input since the DOFs are calculated from the VDOS integral, if you want to rescale the VDOS you can divide the FFT by this integral.
     - removed padding as an input since uniform padding was implimented and does not affect the VDOS integral.
     - removed tau as an input. Current implimentation does not account for VACF rescaling with different tau values. Currently disabled until fixed.
     - VACF is now unnormalized and has units of kg * m^2 / s^2 as expected. 
     - call_fft has been completely re-written to return the spectral density. the units are now kg * m^2 / s, which when divided by kT gives s^1 as expected.
     - added a new input called "length" which allows you to specify how long of a trajectory you want to calculate (i.e. your file is 100 timesteps but you only want to calculate 50)

--- Python Script/QHO_AIMD_entropy.py
+++ Python Script/QHO_AIMD_entropy.py
@@ -4,7 +4,7 @@ Mingze Zheng
 Yanqi Huang
 Geet Gupta
 Johns Hopkins University
-04/12/2024
+08/14/2024
 Version 1.0
 Citations:
 Kabsch rotations: https://github.com/charnley/rmsd
@@ -12,7 +12,14 @@ VACF: Stefan Bringuier https://zenodo.org/records/5261318 DOI: 10.5281/zenodo.52
 QHO: Alexopoulos, K. et al. 10.1021/acs.jpcc.6b00923
 Application: Bukowski B.C. et al. 10.1016/j.jcat.2018.07.012 
 
-v1 update planning
+v1.0 notes:
+ - removed DOFs as an input since the DOFs are calculated from the VDOS integral, if you want to rescale the VDOS you can divide the FFT by this integral.
+ - removed padding as an input since uniform padding was implimented and does not affect the VDOS integral.
+ - removed tau as an input. Current implimentation does not account for VACF rescaling with different tau values. Currently disabled until fixed.
+ - VACF is now unnormalized and has units of kg * m^2 / s^2 as expected. 
+ - call_fft has been completely re-written to return the spectral density. the units are now kg * m^2 / s, which when divided by kT gives s^1 as expected.
+ - added a new input called "length" which allows you to specify how long of a trajectory you want to calculate (i.e. your file is 100 timesteps but you only want to calculate 50)
+
 Important notes - the input file should be pre-processed to only include production data on the adsorbate of interest
 It is your responsibility to ensure the simulation is properly equilibrated - non-equilibrium structures will change the VDOS
 and lead to larger than expect translational, rotational, and vibrational contributions to the VDOS
@@ -28,80 +35,70 @@ i.e. the trajectory should be "unwrapped". There are existing software that can 
 import argparse
 import numpy as np
 from ase.io import read, write
+from ase.visualize import view
 import matplotlib.pyplot as plt
 import scipy
 from scipy import signal
+from scipy.fftpack import fft
+from scipy.fftpack import fftshift
+from scipy.fftpack import fftfreq
 
 
 def parse_arguments():
 
-	parser = argparse.ArgumentParser(
-		description="integrate VDOS obtained from atomic trajectories",
-		usage="Example, QHO_AIMD_entoropy W_g.xyz 0.5 623 --tau 1.1 --mode Vib --pad 15 --dof 3",
-	)
+    parser = argparse.ArgumentParser(
+        description="integrate VDOS obtained from atomic trajectories",
+        usage="Example, QHO_AIMD_entropy.py vasprun.xml 0.5e-15 500 --mode Vib",
+    )
 
-	parser.add_argument(
-		"filename",
-		help="the name of the trajectory file. Standard ASE filetypes are supported",
-		type=str)
+    parser.add_argument(
+        "filename",
+        help="the name of the trajectory file. Standard ASE filetypes are supported",
+        type=str)
 
-	parser.add_argument(
-		"dt",
-		type=float,
-		help="timestep in units of fs",
-	)
+    parser.add_argument(
+        "dt",
+        type=float,
+        help="timestep in units of s",
+    )
 
-	parser.add_argument(
-		"temp",
-		type=float,
-		help="temperature in Kelvin",
-	)
-	parser.add_argument(
-		"--tau",
-		type=float,
-		default=2,
-		help="Correlation time constant to calculate VACF \
-			default is 2, which corresponds to a correlation length of len(trajectory) / 2 \
-			This is usually acceptable",
-	)
+    parser.add_argument(
+        "temp",
+        type=float,
+        help="temperature in Kelvin",
+    )
 
-	parser.add_argument(
-		"--mode",
-		type=str,
-		default=None,
-		help="choose which DoF velocities to analyze \
-			Total -- translation + rotation + vibration \
-			Resid -- rotation + vibration \
-			Vib   -- vibration \
-			keep in mind rotation is ideal gas total rotation. \
-			Recommended usage - for gas-phase molecules use 'Vib' to avoid double-counting rotational modes \
-			- for adsorbate molecules use 'Resid' or 'Total' to use QHO approximation for all modes \
-			unknown strings default to 'Total' and return an error",
-	)
+    parser.add_argument(
+        "length",
+        type=int,
+        help="correlation length you want to calculate \
+            this must be <= the length of the trajectory file \
+            if not specified, this defaults to the total trajectory length",
+    )
 
-	parser.add_argument(
-		"--pad",
-		type=int,
-		default=15,
-		help="zero-padding for FFT function, default 15 is usually acceptable",
-	)
+    parser.add_argument(
+        "--mode",
+        type=str,
+        default=None,
+        help="choose which DoF velocities to analyze \
+            Total -- translation + rotation + vibration \
+            Resid -- rotation + vibration \
+            Vib   -- vibration \
+            keep in mind rotation is ideal gas total rotation. \
+            Recommended usage - for gas-phase molecules use 'Vib' to avoid double-counting rotational modes \
+            - for adsorbate molecules use 'Resid' or 'Total' to use QHO approximation for all modes \
+            unknown strings default to 'Total' and return an error",
+    )
 
-	parser.add_argument(
-		"--dof",
-		type=float,
-		default=None,
-		help="Degrees of freedom to be multiplied by. Defaults to 3N, but it may be necessary to adjust this \
-			for example if calculating QHO modes of a gas phase molecule, you may need to calculate 3N-6 modes\
-			a value of 0 will lead to 3N modes being calculated later",
-	)
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	return args 
+    return args 
 
 def remove_COM_motion(images):
     """
     Takes a set of images, removes their COM motion, and returns the COM velocities
+    unit conversion to true velocity is done in a later step
     
     Returns:
     modified set of images
@@ -200,17 +197,18 @@ def call_vacf(data, correlation_length, masses):
     len_images = len(data[:,0,0])
     total_VACF = np.zeros(correlation_length)
     
-    blocks = range(correlation_length, len_images, correlation_length)
+    blocks = range(correlation_length, len_images+1, correlation_length)
     
     for t in blocks:
         for j, ms in enumerate(masses):
             for i in range(3):
-                total_VACF += autocorr(ms * data[t-correlation_length:t, j, i])
-    total_VACF /= total_VACF[0]
+                total_VACF += autocorr(np.sqrt(ms) * data[t-correlation_length:t, j, i])
+                #total_VACF += autocorr(ms * data[t-correlation_length:t, j, i])
+    total_VACF /= len(blocks)
     time = np.arange(correlation_length)
     return time, total_VACF
 
-def call_fft(vacf, pad_factor, dt, correlation):
+def call_fft(vacf, dt, correlation):
     """
     Takes a VACF and returns the VDOS. Note this is set up to use a Hanning window
     There are many possible window functions in discrete signal processing that can be used
@@ -225,21 +223,21 @@ def call_fft(vacf, pad_factor, dt, correlation):
     
     Returns:
     unnormalized VDOS for a given VACF using a Hanning window function
-    
-    algorithm adapted from: 
-    VACF: Stefan Bringuier https://zenodo.org/records/5261318 DOI: 10.5281/zenodo.5261318  
     """ 
-    def fft_autocorr(AutoCorr, dt):
-        fft_arry = np.fft.rfft(AutoCorr, norm="ortho") * dt
-        return fft_arry    
+    vacf = vacf * np.hanning(vacf.size)
+    og_nf = len(vacf)
+    nfft = 25*og_nf # 25x zero pad - can be modified
+    nfft2 = int(nfft/2)
+    X = fft(vacf, nfft) /og_nf
+    Xfreq = fftfreq(nfft, d=dt)
+    Xfreq *= (1/299792458)/100 # convert from s^-1 to cm^-1 for plotting
+    Xfreq = Xfreq[:nfft2]
+    X = X[0:nfft2]
+    X[1:nfft2] = 2*X[1:nfft2] # FFT symmetry
     
-    windowed_vacf = vacf * np.hanning(vacf.size)
-    windowed_vacf_v = np.lib.pad(windowed_vacf, (0, vacf.size * pad_factor), "constant", constant_values=(0))
-    freq = np.fft.rfftfreq(windowed_vacf_v.size, d=dt)*(1/299792458) /100 # convert to cm-1
-    fft_vacf = np.abs(fft_autocorr(windowed_vacf_v, dt))
-    return freq, fft_vacf
+    return Xfreq, np.abs(X)
 
-def integrate_VDOS(fft_normed, freq, T, DoFs):
+def integrate_VDOS(fft_normed, freq, T):
     """
     Takes a normalized VDOS, then integrates with a QHO weight function.
     
@@ -254,88 +252,92 @@ def integrate_VDOS(fft_normed, freq, T, DoFs):
     Bukowski B.C. et al. 10.1016/j.jcat.2018.07.012 
     """     
     def weight_function(freqs, T):
-        hbar = 6.62607015E-34
+        hbar = 6.62607015E-34 # J/K
         kT = (1.380649E-23*T) # J
         x = hbar * freqs / (2*kT)
         return x/np.tanh(x) - np.log(2*np.sinh(x))
 
-    freq_s = freq * 3e10
+    freq_s = freq * 3e10 # convert back to s^-1 for integration
     set_weighted = fft_normed[1:] * weight_function(freq_s[1:], T)
-    return np.trapz(set_weighted, x=freq[1:]) * DoFs * 8.314
+    return np.trapz(set_weighted, x=freq[1:]) * 1 * 8.314 # R in J/mol/K
 
 
 def main():
-	args = parse_arguments()
-	MD_filepath = args.filename
-	tau = args.tau 
-	vacf_mode = args.mode
-	dt = args.dt 
-	z_padding = args.pad 
-	T = args.temp 
-	DoFs = args.dof 
+    args = parse_arguments()
+    MD_filepath = args.filename
+    vacf_mode = args.mode
+    dt = args.dt 
+    T = args.temp 
+    length = args.length
 
-	if vacf_mode == None:
-		vacf_mode = "Total"
-		print("Defaulting to using the total trajectories to make the VDOS") 
+    if vacf_mode == None:
+        vacf_mode = "Total"
+        print("Defaulting to using the total trajectories to make the VDOS")
 
-	images = read(MD_filepath, index=":")
+    #len - 
 
-	# remove COM motion and calculate COM and total velocities
-	images, V_COM, V_total = remove_COM_motion(images)
-	# V_resid is the residual velocity after removing COM motion
-	V_resid = V_total - V_COM
-	# remove principal rotations to calculate vibrational velocity.
-	# images are modified in-place
-	images, V_vib = remove_rotational_motion(images)
+    images = read(MD_filepath, index=f"-{length}:")
+    #images = images[:length]
+    print(len(images))
 
-	# ncorr is the length of the VACF. Typically, longer is better
-	# though you want some blocks to average over. 
-	# I recommend tuning this value to find a converged result
-	ncorr = int(np.floor(len(images) / tau))
-	masses = images[0].get_masses()
+    # remove COM motion and calculate COM and total velocities
+    images, V_COM, V_total = remove_COM_motion(images)
+    # V_resid is the residual velocity after removing COM motion
+    V_resid = V_total - V_COM
+    # remove principal rotations to calculate vibrational velocity.
+    # images are modified in-place
+    images, V_vib = remove_rotational_motion(images)
 
-	if vacf_mode == "Vib":
-	    times, VACF = call_vacf(V_vib, ncorr, masses)
-	elif vacf_mode == "Resid":
-	    times, VACF = call_vacf(V_resid, ncorr, masses)
-	elif vacf_mode == "Total":
-	    times, VACF = call_vacf(V_total, ncorr, masses)
-	else:
-	    times, VACF = call_vacf(V_total, ncorr, masses) 
+    # clean this section up
+    dt_au = dt*1e15 # convert from s to fs
+    V_vib /= dt_au # divide by dt to get velocity in A/fs
+    V_vib *= 1.0E5 # convert A/fs to m/s
+    V_resid /= dt_au
+    V_resid *= 1.0E5
+    V_total /= dt_au
+    V_total *= 1.0E5
+    masses = images[0].get_masses()
 
-	plt.plot(VACF)
-	plt.title("VACF")
-	plt.xlabel("correlation time / a.u.")
-	plt.ylabel("normed VACF / a.u.")
-	plt.show()
+    ncorr = len(images)
+    masses = images[0].get_masses()
+
+    if vacf_mode == "Vib":
+        times, VACF = call_vacf(V_vib, ncorr, masses*1.66054e-27)
+    elif vacf_mode == "Resid":
+        times, VACF = call_vacf(V_resid, ncorr, masses*1.66054e-27)
+    elif vacf_mode == "Total":
+        times, VACF = call_vacf(V_total, ncorr, masses*1.66054e-27)
+    else:
+        times, VACF = call_vacf(V_total, ncorr, masses*1.66054e-27) 
+
+    plt.plot(VACF)
+    plt.title("VACF")
+    plt.xlabel("correlation time / s")
+    plt.ylabel("VACF / kg m^2 / s^2")
+    plt.show()
 
 
-	# create the initial VDOS called fft
-	# due to spectral leakage, there may be a nonzero fft[0]
-	# this is expected for a diffusing system, but problematic for vibrational modes
-	# here we background subtract and save it as fft_bkg
-	# the FFT is then normalized to 1 for integration
-	dt *= 1e-15 # convert to s
-	freq, fft = call_fft(VACF, z_padding, dt, ncorr)
-	fft_bkg = fft - (fft[0] / (1 + (freq)**2))
-	fft_normed = fft_bkg / np.trapz(fft_bkg, x=freq)
+    freq, ifft = call_fft(VACF, dt, ncorr)
+    ifft *= dt #multiply fft bins by dt for spectral density
 
-	if DoFs == None:
-		DoFs = 3*len(images[0])
-		print(f"Defaulting to 3N DoFs, in this case {int(DoFs)}")
+    kt = 1.380649E-23 * T
+    ifft *= (2/kt) # normalize by KE
+    ifft *= 2.998E10 # convert to cm so that x axis is cm-1 and y axis is cm
+    ifft *= 1.63 # Hanning window correction factor from Discrete Signal Processing
+    nDoF = np.trapz(ifft[:], x=freq[:]) # integral DOS which should equal nDoF
+    entropy = integrate_VDOS(ifft, freq, T)
+    print(f"Entropy at {T} K with {nDoF} degrees of freedom: {entropy} J Mol-1 K-1")
 
-	entropy = integrate_VDOS(fft_normed, freq, T, DoFs)
-	print(f"Entropy at {int(T)} K with {int(DoFs)} degrees of freedom: {np.round(entropy,2)} J Mol-1 K-1")
 
-	plt.plot(freq, fft_normed)
-	plt.title("VDOS")
-	plt.xlim(0, 4500)
-	plt.ylim(0, np.max(fft_normed))
-	plt.xlabel("cm^-1")
-	plt.ylabel("normalized intensity")
-	plt.show()
+    plt.plot(freq, ifft)
+    plt.title("VDOS")
+    plt.xlim(0, 4500)
+    plt.ylim(0, np.max(ifft))
+    plt.xlabel("cm^-1")
+    plt.ylabel("cm")
+    plt.show()
 
-	return None
+    return None
 
 if __name__ == "__main__":
-	main()
+    main()
